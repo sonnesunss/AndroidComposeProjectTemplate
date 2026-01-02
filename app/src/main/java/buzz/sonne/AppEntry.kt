7babad08a07c7bad3073888327e3da6ee335b711
dora <145575806+sonnesunss@users.noreply.github.com>
2026-01-02 21:06:28 +0800

    添加Hilt基本支持注入逻辑

---
+++ app/src/main/java/buzz/sonne/AppEntry.kt
@@ -0,0 +1,8 @@
+package buzz.sonne
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppEntry : Application() {
+}
--- app/src/main/java/buzz/sonne/MainActivity.kt
+++ app/src/main/java/buzz/sonne/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import buzz.sonne.ui.theme.AndroidComposeProjectTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
